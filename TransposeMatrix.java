a157ecead473e58e42d8a771d689a348877a6a36
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-11-06 10:58:41 -0500

    printMatrix method finished

--- TransposeMatrix.java
+++ TransposeMatrix.java
@@ -39,4 +39,13 @@ public class TransposeMatrix {
 
         return transposedMatrix;
     }
+
+    private static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++ ) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.println(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
