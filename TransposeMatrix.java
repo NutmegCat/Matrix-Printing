8cb4bceab53920c505a2ecf866f68f20e0cc6576
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-11-06 11:08:22 -0500

    deprecated

--- TransposeMatrix.java
+++ TransposeMatrix.java
@@ -40,6 +40,7 @@ public class TransposeMatrix {
         return transposedMatrix;
     }
 
+    //! This does not print out like a grid format
     private static void printMatrix(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
